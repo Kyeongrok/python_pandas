2add14ca36a520d2a81a5ff34650f95c0fe17e5b
kyeongrok <oceanfog1@gmail.com>
2019-11-06 18:38:49 +0900

    add year

--- com/03_data_work/02_add_year.py
+++ com/03_data_work/02_add_year.py
@@ -1,6 +1,7 @@
 # year.txt를 읽어와서 map으로 만든다.
 file = open("./year.txt")
 lines = file.readlines()
+file.close()
 
 headerYear = {}
 for line in lines:
@@ -12,6 +13,21 @@ print(headerYear)
 
 # c_nt.fas에 year추가
 # line을 돌다가 map의 key에 값이 존재하면 옆에 ,year를 찍음
+
+file = open("./C_nt.fas")
+targetFile = open("addYear.txt","w+")
+lines = file.readlines()
+
+for line in lines:
+    line = line.replace(">", "").replace("\n","")
+    if line in headerYear.keys():
+        string = ">{},{}\n".format(line, headerYear[line])
+        targetFile.write(string)
+    else:
+        targetFile.write("{}\n".format(line))
+
+
+
 
 
 
